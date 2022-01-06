db56e94c0414f26b63e9880a12d0dd67e771d22e
martinsstuff <martinsstuff@users.noreply.github.com>
2022-01-06 20:07:15 +0100

    Update download script

--- dl.sh
+++ dl.sh
@@ -3,6 +3,7 @@ mirror=https://raw.githubusercontent.com/martinsstuff/nixcfg/dev/
 
 curl -O ${mirror}nixOSFileystemSetup.sh
 curl -O ${mirror}nixOSPartitionSetup.sh
+curl -O ${mirror}configuration.nix
 
 chmod +x nixOSFileystemSetup.sh
 chmod +x nixOSPartitionSetup.sh
